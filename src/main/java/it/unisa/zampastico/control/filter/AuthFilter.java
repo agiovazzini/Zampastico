110b47542a28deb35650f35b0d107fcbff927c79
Antonio Ruotolo <a.ruotolo20@studenti.unisa.it>
2026-06-02 22:10:34 +0200

    Implementati filtri per la gestione dell'autenticazione

--- src/main/java/it/unisa/zampastico/control/filter/AuthFilter.java
+++ src/main/java/it/unisa/zampastico/control/filter/AuthFilter.java
@@ -2,22 +2,29 @@ package it.unisa.zampastico.control.filter;
 
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
 
 @WebFilter(urlPatterns = {
     "/checkout", "/orders-history", "/profile", "/wishlist"
 })
-public class AuthFilter implements Filter {
+public class AuthFilter extends HttpFilter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
 
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
+    	HttpSession session = request.getSession(false);
 
-        res.sendRedirect(req.getContextPath() + "/login?error=login_required");
+        if (session == null || session.getAttribute("utenteLoggato") == null) {
+            response.sendRedirect(request.getContextPath() + "/login?errore=login_required");
+            return;
+        }
+
+        chain.doFilter(request, response);
     }
 }
