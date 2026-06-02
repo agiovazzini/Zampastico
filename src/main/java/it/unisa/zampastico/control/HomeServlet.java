ec93383b8341f6e00b9447f86d29bfab9f9c212a
Antonio Ruotolo <a.ruotolo20@studenti.unisa.it>
2026-06-02 20:22:36 +0200

    Modificata la HomeServlet per lo statoAccesso

--- src/main/java/it/unisa/zampastico/control/HomeServlet.java
+++ src/main/java/it/unisa/zampastico/control/HomeServlet.java
@@ -6,21 +6,23 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
-    
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public HomeServlet() {
-        super();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {   
+        HttpSession session = request.getSession();
+        String info = (String) session.getAttribute("info");
+        if (info != null) {
+        	request.setAttribute("statoAccesso", info);
+        	session.removeAttribute("info");
+        	request.getRequestDispatcher("/WEB-INF/view/home.jsp").forward(request, response);
+        }
     }
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    request.getRequestDispatcher("/WEB-INF/view/home.jsp").forward(request, response);
-	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 }
