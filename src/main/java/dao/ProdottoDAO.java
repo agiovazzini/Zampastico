d1957a07446c843609d3043d5a7ed066b8142321
AlbertoUni <a.giovazzini@studenti.unisa.it>
2026-06-25 11:15:58 +0200

    Creazione Interfaccia ProdottoDAO.java

---
+++ src/main/java/dao/ProdottoDAO.java
@@ -0,0 +1,12 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+import model.ProdottoBEAN;
+
+public interface ProdottoDAO {
+	public void doSave(ProdottoBEAN prodotto) throws SQLException;
+    public boolean doDelete(int id) throws SQLException;
+    public ProdottoBEAN doRetrieveByKey(int id) throws SQLException;
+    public List<ProdottoBEAN> doRetrieveAll(String orderBy) throws SQLException;
+}
