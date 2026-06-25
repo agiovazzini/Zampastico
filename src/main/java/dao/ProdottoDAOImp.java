f1968382acca0e643c9b6603d1c12b7cd72558f1
Antonio Ruotolo <a.ruotolo20@studenti.unisa.it>
2026-06-25 22:57:44 +0200

    Implementazione prodotto fix su delete

--- src/main/java/dao/ProdottoDAOImp.java
+++ src/main/java/dao/ProdottoDAOImp.java
@@ -4,12 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
 import model.ProdottoBEAN;
 
 public class ProdottoDAOImp implements ProdottoDAO {
+	private static final String TABLE_NAME = "Prodotto";
     private DataSource ds;
 
     public ProdottoDAOImp(DataSource ds) {
@@ -17,12 +19,43 @@ public class ProdottoDAOImp implements ProdottoDAO {
     }
     
     @Override
-    public void doSave(ProdottoBEAN prodotto) throws SQLException {
+    public synchronized void doSave(ProdottoBEAN prodotto) throws SQLException {
+        String sql = "INSERT INTO " + TABLE_NAME + " (nome, descrizione, brand, id_categoria, attivo) VALUES (?, ?, ?, ?, ?)";
+        try (Connection con = ds.getConnection(); 
+             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, prodotto.getNome());
+            ps.setString(2, prodotto.getDescrizione());
+            ps.setString(3, prodotto.getMarca());
+            ps.setInt(4, prodotto.getIdCategoria());
+            ps.setBoolean(5, prodotto.isAttivo());         
+            ps.executeUpdate();
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    prodotto.setId(rs.getInt(1));
+                }
+            }
+        }
+    }
+    
+    // Aggiornamento
+    public synchronized boolean doUpdate(ProdottoBEAN prodotto) throws SQLException {
+        String sql = "UPDATE " + TABLE_NAME + " SET nome = ?, descrizione = ?, brand = ?, id_categoria = ?, attivo = ? WHERE id_prodotto = ?";
+        try (Connection con = ds.getConnection(); 
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, prodotto.getNome());
+            ps.setString(2, prodotto.getDescrizione());
+            ps.setString(3, prodotto.getMarca());
+            ps.setInt(4, prodotto.getIdCategoria());
+            ps.setBoolean(5, prodotto.isAttivo());
+            ps.setInt(6, prodotto.getId());
+            
+            return ps.executeUpdate() > 0;
+        }
     }
     
     @Override
     public boolean doDelete(int id) throws SQLException {
-        String sql = "UPDATE Prodotto SET eliminato = TRUE WHERE id_prodotto = ?";
+        String sql = "UPDATE Prodotto SET attivo = TRUE WHERE id_prodotto = ?";
         try (Connection con = ds.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setInt(1, id);
             return ps.executeUpdate() > 0;
@@ -43,7 +76,7 @@ public class ProdottoDAOImp implements ProdottoDAO {
                      "JOIN VarianteProdotto vp ON p.id_prodotto = vp.id_prodotto " +
                      "JOIN Categoria c ON p.id_categoria = c.id_categoria " +
                      "LEFT JOIN ImmagineVariante iv ON vp.id_varianteProdotto = iv.id_varianteProdotto AND iv.immagine_copertina = TRUE " +
-                     "WHERE p.eliminato = FALSE";
+                     "WHERE p.attivo = FALSE";
         
         try (Connection con = ds.getConnection(); 
              PreparedStatement ps = con.prepareStatement(sql); 
@@ -59,6 +92,29 @@ public class ProdottoDAOImp implements ProdottoDAO {
                 p.setImmagine(rs.getString("url_immagine"));
                 p.setCategoria(rs.getString("categoria"));
                 lista.add(p);
+            }
+        }
+        return lista;
+    }
+    
+    public List<ProdottoBEAN> doRetrieveByCategoria(int idCategoria) throws SQLException {
+        List<ProdottoBEAN> lista = new ArrayList<>();
+        String sql = "SELECT * FROM " + TABLE_NAME + " WHERE id_categoria = ?";
+        try (Connection con = ds.getConnection(); 
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, idCategoria);
+            
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    ProdottoBEAN p = new ProdottoBEAN();
+                    p.setId(rs.getInt("id_prodotto"));
+                    p.setNome(rs.getString("nome"));
+                    p.setDescrizione(rs.getString("descrizione"));
+                    p.setMarca(rs.getString("brand"));
+                    p.setIdCategoria(rs.getInt("id_categoria"));
+                    p.setAttivo(rs.getBoolean("attivo"));
+                    lista.add(p);
+                }
             }
         }
         return lista;
