c720366c4c195988bd0b5d2901af98bab4ec7915
Antonio Ruotolo <a.ruotolo20@studenti.unisa.it>
2026-06-25 17:11:03 +0200

    Creazione del bean di COUPON

---
+++ src/main/java/model/CouponBEAN.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class CouponBEAN implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int idCoupon;
+    private String codice;
+    private double percentualeSconto;
+    private LocalDateTime dataScadenza;
+
+    public CouponBEAN() {
+    }
+
+    public int getIdCoupon() {
+        return idCoupon;
+    }
+
+    public void setIdCoupon(int idCoupon) {
+        this.idCoupon = idCoupon;
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public void setCodice(String codice) {
+        this.codice = codice;
+    }
+
+    public double getPercentualeSconto() {
+        return percentualeSconto;
+    }
+
+    public void setPercentualeSconto(double percentualeSconto) {
+        this.percentualeSconto = percentualeSconto;
+    }
+
+    public LocalDateTime getDataScadenza() {
+        return dataScadenza;
+    }
+
+    public void setDataScadenza(LocalDateTime dataScadenza) {
+        this.dataScadenza = dataScadenza;
+    }
+}
