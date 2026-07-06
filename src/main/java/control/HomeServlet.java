fd7e4e24eb4166ef05286372c952de71676d8bc1
Antonio Ruotolo <a.ruotolo20@studenti.unisa.it>
2026-07-06 19:27:04 +0200

    Aggiornata la servlet per le categorie dinamiche dell'header

--- src/main/java/control/HomeServlet.java
+++ src/main/java/control/HomeServlet.java
@@ -1,16 +1,39 @@
 package control;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import javax.sql.DataSource;
+
+import dao.CategoriaDAOImp;
+import dao.ProdottoDAOImp;
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import model.CategoriaBEAN;
+import model.ProdottoBEAN;
 
 @WebServlet("/home")
 public class HomeServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    
+    private CategoriaDAOImp categoriaDAO;
+    private ProdottoDAOImp prodottoDAO;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        DataSource ds = (DataSource) getServletContext().getAttribute("DataSource");
+        if (ds == null) {
+            throw new ServletException("DataSource non disponibile nel contesto");
+        }
+        categoriaDAO = new CategoriaDAOImp(ds);
+        prodottoDAO = new ProdottoDAOImp(ds);
+    }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {   
         HttpSession session = request.getSession();
@@ -19,6 +42,20 @@ public class HomeServlet extends HttpServlet {
         	request.setAttribute("statoAccesso", info);
         	session.removeAttribute("info");
         }
+        
+        try {
+            List<CategoriaBEAN> tutteLeCategorie = categoriaDAO.doRetrieveAll();
+            request.setAttribute("categorieHome", tutteLeCategorie);
+            List<ProdottoBEAN> tuttiProdotti = prodottoDAO.doRetrieveAll("");
+            List<ProdottoBEAN> prodottiInEvidenza = tuttiProdotti.size() > 4 
+                    ? tuttiProdotti.subList(0, 4) 
+                    : tuttiProdotti;
+            request.setAttribute("prodottiHome", prodottiInEvidenza);
+            
+        } catch (SQLException e) {
+           return;
+        }
+
         request.getRequestDispatcher("/WEB-INF/view/common/home.jsp").forward(request, response);
     }
 
